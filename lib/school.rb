229e38314d1b909e5e9e40fe4469ac8726a978e2
Nattida Wong <nattidac.wong@gmail.com>
2020-09-26 02:49:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,12 @@
 # code here!
 class School
-  attr_accessor :name
+  attr_accessor :name, :roster
 
   def initialize(name)
     @name = name
+    @roster = {}
   end
 
+  def add_student(student_name, grade)
+    
 end
